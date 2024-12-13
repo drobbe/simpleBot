b78d5cc8a89cf7c911bd7e80651a38c16bd07b92
Edgard <edgard@gmail.com>
2024-12-13 15:18:54 +0100

    subida

---
+++ bot.js
@@ -0,0 +1,88 @@
+import { Client, Events } from "discord.js";
+import {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  VoiceConnectionStatus,
+} from "@discordjs/voice";
+import path from "path";
+const __dirname = path.resolve();
+
+export class BotClass {
+  userSounds = {
+    "drobbe.": "test.mp3", // Reemplaza USER_ID_1 con el ID del usuario
+    hunterobot: "venao.mp3", // Otro usuario más
+    "⎝kratek ᵛᵉ⎠": "kratek.mp3", // Otro usuario
+    gastaclaux: "elio.mp3", // Otro usuario más
+    sazecj: "campanas.mp3", // Otro usuario más
+  };
+
+  constructor() {
+    this.client = new Client({
+      intents: 53608447,
+    });
+  }
+
+  start() {
+    this.client.login(process.env.TOKEN_GG);
+    this.listenEvents();
+  }
+
+  listenEvents() {
+    this.client.on("ready", async () => {
+      console.log(`Encendido como ${this.client.user.username}`);
+    });
+
+    this.client.on(Events.MessageCreate, async (message) => {
+      console.log(message.content);
+      if (message.content === "ping") {
+        message.reply("pong");
+      }
+    });
+
+    this.client.on(Events.MessageCreate, async (message) => {
+      console.log(message.content);
+      if (message.content === "hola") {
+        message.reply("Buenos dias");
+      }
+    });
+
+    this.client.on("voiceStateUpdate", (oldState, newState) => {
+      if (!oldState.channel && newState.channel) {
+        const userTag = newState.member.user.tag;
+
+        console.log(`${userTag} joined a voice channel.`);
+
+        const sound = this.userSounds[userTag];
+
+        if (!sound) return;
+        try {
+          // Unirse al canal de voz
+          const connection = joinVoiceChannel({
+            channelId: newState.channel.id,
+            guildId: newState.guild.id,
+            adapterCreator: newState.guild.voiceAdapterCreator,
+          });
+
+          // Reproducir sonido al estar listo
+          connection.once(VoiceConnectionStatus.Ready, () => {
+            console.log("Connected to the voice channel!");
+            const player = createAudioPlayer();
+            const SOUND_PATH = path.join(__dirname, sound);
+
+            const resource = createAudioResource(SOUND_PATH);
+
+            player.play(resource);
+            connection.subscribe(player);
+
+            player.on("idle", () => {
+              connection.destroy(); // Salir del canal tras reproducir el sonido
+            });
+          });
+        } catch (err) {
+          console.error("Error connecting to the voice channel:", err);
+        }
+      }
+    });
+  }
+}
--- index.js
+++ index.js
@@ -1,88 +1,7 @@
-const express = require("express");
+import express from "express";
+import { BotClass } from "./bot.js";
 const app = express();
 const port = 3000;
-
-const { Client, GatewayIntentBits, Events } = require("discord.js");
-const {
-  joinVoiceChannel,
-  createAudioPlayer,
-  createAudioResource,
-  VoiceConnectionStatus,
-} = require("@discordjs/voice");
-const path = require("path");
-
-const userSounds = {
-  "drobbe.": "test.mp3", // Reemplaza USER_ID_1 con el ID del usuario
-  //   hunterobot: "kratek.mp3", // Otro usuario más
-  "⎝kratek ᵛᵉ⎠": "kratek.mp3", // Otro usuario
-  gastaclaux: "elio.mp3", // Otro usuario más
-  sazecj: "campanas.mp3", // Otro usuario más
-};
-
-const client = new Client({
-  intents: 53608447,
-});
-
-client.on("ready", async () => {
-  console.log(`Encendido como ${client.user.username}`);
-});
-
-client.login(process.env.TOKEN_GG);
-
-client.on(Events.MessageCreate, async (message) => {
-  console.log(message.content);
-  if (message.content === "ping") {
-    message.reply("pong");
-  }
-});
-
-client.on(Events.MessageCreate, async (message) => {
-  console.log(message.content);
-  if (message.content === "hola") {
-    message.reply("Buenos dias");
-  }
-});
-
-client.on("voiceStateUpdate", (oldState, newState) => {
-  // Check if a member joined a voice channel
-  // Verifica si alguien se unió a un canal de voz
-
-  if (!oldState.channel && newState.channel) {
-    const userTag = newState.member.user.tag;
-
-    console.log(`${userTag} joined a voice channel.`);
-
-    const sound = userSounds[userTag];
-
-    if (!sound) return;
-    try {
-      // Unirse al canal de voz
-      const connection = joinVoiceChannel({
-        channelId: newState.channel.id,
-        guildId: newState.guild.id,
-        adapterCreator: newState.guild.voiceAdapterCreator,
-      });
-
-      // Reproducir sonido al estar listo
-      connection.once(VoiceConnectionStatus.Ready, () => {
-        console.log("Connected to the voice channel!");
-        const player = createAudioPlayer();
-        const SOUND_PATH = path.join(__dirname, sound);
-
-        const resource = createAudioResource(SOUND_PATH);
-
-        player.play(resource);
-        connection.subscribe(player);
-
-        player.on("idle", () => {
-          connection.destroy(); // Salir del canal tras reproducir el sonido
-        });
-      });
-    } catch (err) {
-      console.error("Error connecting to the voice channel:", err);
-    }
-  }
-});
 
 app.get("/", (req, res) => {
   res.send("Bot arriba");
@@ -103,3 +22,7 @@ setInterval(async () => {
     console.log(err.message); //can be console.error
   }
 }, 1000 * 60 * 10);
+
+const mamCulBot = new BotClass();
+
+mamCulBot.start();
